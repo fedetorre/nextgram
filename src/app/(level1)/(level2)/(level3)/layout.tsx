16413ccfdb78f5bb8099ee78ac3f625d04bab050
Federico Torresan <federico.torresan@quentral.com>
2023-12-05 09:55:25 +0100

    demo for intercept route fix

---
+++ src/app/(level1)/(level2)/(level3)/layout.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import React from "react";
+
+export default function Layout(props: { children: React.ReactNode, modal: React.ReactNode }) {
+	return (<>
+		{props.children}
+		{props.modal}
+	</>);
+};
